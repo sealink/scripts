768d946aa7d0f9f25add0d05b520c8880519e49e
Michael Noack <michael@noack.com.au>
2012-01-04 16:32:38 +1030

    EC2 Server Setup/Disk Resize script

---
+++ server_setup.rb
@@ -0,0 +1,77 @@
+class Server
+  attr_accessor :server, :compute
+  def initialize(compute)
+    @compute = compute
+  end
+end
+
+DB_PASSWORD       = 'test'
+FLAVOR = 'm1.large'
+#FLAVOR = 't1.micro'
+AMI    = 'ami-4af18918'
+ZONE   = 'ap-southeast-1b'
+
+class AmazonServer < Server
+  def create
+    @server = compute.servers.create(
+      :image_id          => AMI,
+      :flavor_id         => FLAVOR,
+      :subnet_id         => 'subnet-7300e71a', # Virtual Private Cloud ID
+      :key_name          => 'michael', # e.g. michael.pem
+      :availability_zone => ZONE,
+      :groups            => ['default'],
+      :tags              => {'name' => 'FogTest'},
+      :private_key_path  => '/home/michael/michael.pem'
+    )
+    puts @server.inspect
+    @server
+  end
+
+  # Resizes a disk
+  # new_size: string
+  def resize_disk(new_size)
+    server.stop
+    puts "stopping server"
+    server.wait_for{print "."; state == 'stopped'}
+    
+    
+    small_disk = server.volumes.first
+
+    snapshot_id = compute.create_snapshot(small_disk.id, "Snapshot for FOG TEST").body['snapshotId']
+    puts "crating snapsnot"
+    compute.snapshots.get(snapshot_id).wait_for { print "."; ready? }
+    big_disk = compute.volumes.create(:availability_zone => ZONE, :size => new_size, :snapshot_id => snapshot_id)
+    puts "create volume from snap"
+    compute.volumes.get(big_disk.id).wait_for { print "."; state == 'available'}
+    big_disk.reload
+
+    small_disk.server = nil
+    small_disk.destroy
+    big_disk.device = '/dev/sda1'
+    big_disk.server = server
+    server.start
+    puts "starting server"
+    server.wait_for{ print "."; ready?}
+    puts "started"
+
+    server.ssh("sudo resize2fs /dev/sda1")
+  end
+end
+
+class VirtualBoxServer < Server
+  def create
+    compute.servers.create(
+      :name => 'App Server2',
+      :os => 'Ubuntu')
+    medium = compute.mediums.create(
+      :device_type => :hard_disk,
+      :location => '/home/michael/VirtualBox VMs/App Server/NewHardDisk1.vdi',
+      :read_only => false)
+    storage_controller = server.storage_controllers.create(
+      :bus => :sata, :name => 'sata')
+    storage_controller.attach(medium, 0)
+
+    network_adapter = server.network_adapters.first
+    network_adapter
+  end
+end
