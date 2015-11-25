b390a229dbd47e6369ea48801ea09012e04caf12
Michael Smirnoff <michaelsmirnoff@live.com>
2015-11-25 15:10:46 +1030

    Correct scoping

--- deploy/deployable/eb_platform.rb
+++ deploy/deployable/eb_platform.rb
@@ -1,6 +1,6 @@
 class EBPlatform
   def deploy(application)
-    fail "Environment NOT READY!" unless application.platform_config.ready?
-    system("eb-deploy-tag #{TAG}")
+    @application = application
+    fail "Environment NOT READY!" unless @application.platform_config.ready?
   end
 end
