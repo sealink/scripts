fb418b082c56dc282e0a0f2b3b501535457bd3d9
Michael Smirnoff <michaelsmirnoff@live.com>
2015-11-26 16:47:19 +1030

    Make Demeter happier

--- deploy/deployable/eb_platform.rb
+++ deploy/deployable/eb_platform.rb
@@ -1,6 +1,13 @@
 class EBPlatform
   def deploy(application)
     @application = application
-    fail "Environment NOT READY!" unless @application.platform_config.ready?
+    fail "Environment NOT READY!" unless eb.ready?
+  end
+
+  private
+  def eb
+    @eb ||= @application.platform_config
+  end
+
   end
 end
