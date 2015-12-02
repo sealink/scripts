1a11c28d4b01872f96d6cdac7e1c6e59ba1a3ac3
Michael Smirnoff <michaelsmirnoff@live.com>
2015-12-02 14:09:27 +1030

    Place S3 classes in S3 common module

---
+++ deploy/s3/platform.rb
@@ -0,0 +1,22 @@
+module S3
+  class Platform
+    def deploy(application)
+      @application = application
+      @tag = @application.tag
+      s3_deploy!
+    end
+
+    private
+    def s3
+      @s3 ||= @application.platform_config
+    end
+
+    def s3_deploy!
+      system(
+        "bucket=#{s3.target_bucket}"\
+        " s3_config_version=#{s3.version}"\
+        " npm run publish"
+      )
+    end
+  end
+end
