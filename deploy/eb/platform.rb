f016e0c6f7f157724bcdbf40f831367da4d50bfc
Michael Smirnoff <michaelsmirnoff@live.com>
2015-12-03 15:02:22 +1030

    Simplify EB redeployment checks massively

--- deploy/eb/platform.rb
+++ deploy/eb/platform.rb
@@ -7,7 +7,6 @@ module Eb
 
     def deploy!
       fail "Environment NOT READY!" unless @eb.ready?
-      check_version
       eb_deploy!
     end
 
@@ -19,24 +18,15 @@ module Eb
       ! @eb.response.application_versions.empty?
     end
 
-    def check_version
-      puts "Checking the Beanstalk..."
-      if eb_label_exists?
-        puts "Elastic Beanstalk application #{@eb.application_name}"\
-             " already has version #{@tag}"
-        puts "Assuming you do mean to redeploy, perhaps to a new target."
-        @use_existing = true
-      else
-        puts "Elastic Beanstalk doesn't have this version but tag exists."\
-             "This usually indicates a recently failed deployment."
-        puts "THIS SHOULD NOT HAPPEN. DEPLOY ANYWAY? (Y/N)"
-        yoloswag = STDIN.gets
-        fail 'Deployment killed, fix your repo' unless yoloswag.start_with?('y')
-      end
+    def write_redeploy_notification
+      puts "Elastic Beanstalk application #{@eb.application_name}"\
+           " already has version #{@tag}"
+      puts "Assuming you do mean to redeploy, perhaps to a new target."
     end
 
     def eb_deploy!
-      if @use_existing
+      if eb_label_exists?
+        write_redeploy_notification
         system("eb deploy --version=#{tag}")
       else
         system("eb deploy --label=#{tag}")
